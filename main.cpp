f96d93798b6353ca7d2fae42d2afc4904936aaf4
Jonnas Costa <jonn567@gmail.com>
2021-11-08 21:00:25 +0000

    stable version main

--- main.cpp
+++ main.cpp
@@ -3,22 +3,21 @@
 #include <time.h>   /* time */
 #include <SFML/Graphics.hpp>
 #include <random>
-#include "./classes/GameState.h"
+//#include "./classes/GameState.h"
 
 #define WIDTH 800
 #define HEIGHT 600
 
 using namespace sf;
-using namespace gg;
+//using namespace gg;
 
-GameState game;
+//GameState game;
 
 void init()
 {
-    RenderWindow window(VideoMode(WIDTH, HEIGHT), "SFML works!");
-    window.setVerticalSyncEnabled(true); // call it once, after creating the window
-    RenderWindow &wnd_p = window;
-    game.set_render_window(&wnd_p);
+   
+    //RenderWindow &wnd_p = window;
+    //game.set_render_window(&wnd_p);
 }
 
 void draw()
@@ -32,54 +31,55 @@ void update()
 int main()
 {
     init();
-    RenderWindow *window = game.get_render_window();
-    std::vector<CircleShape> shapes_list;
-    int iSecret, icolor, iGuess;
-    float rsize;
-    float x, y;
-    rsize = (float(rand()) / float((RAND_MAX)) * 100);
+    // RenderWindow *window = game.get_render_window();
+    // std::vector<CircleShape> shapes_list;
+    // int iSecret, icolor, iGuess;
+    // float rsize;
+    // float x, y;
+    // rsize = (float(rand()) / float((RAND_MAX)) * 100);
 
-    srand(time(NULL));
-    /* generate secret number between 1 and 10: */
-    rsize = rand() % 100;
-    shapes_list.push_back(CircleShape(rsize));
-    shapes_list.push_back(CircleShape(rsize));
-    shapes_list.push_back(CircleShape(rsize));
-    shapes_list.push_back(CircleShape(rsize));
+    // srand(time(NULL));
+    // /* generate secret number between 1 and 10: */
+    // rsize = rand() % 100;
+    // shapes_list.push_back(CircleShape(rsize));
+    // shapes_list.push_back(CircleShape(rsize));
+    // shapes_list.push_back(CircleShape(rsize));
+    // shapes_list.push_back(CircleShape(rsize));
 
-    Color colors_shapes[] = {Color::Magenta,
-                             Color::Red,
-                             Color::Blue};
-
-    while (window->isOpen())
+    // Color colors_shapes[] = {Color::Magenta,
+    //                          Color::Red,
+    //                          Color::Blue};
+    RenderWindow window(VideoMode(WIDTH, HEIGHT), "SFML works!");
+    window.setVerticalSyncEnabled(true); // call it once, after creating the window
+    while (window.isOpen())
     {
         Event event;
-        while (window->pollEvent(event))
+        while (window.pollEvent(event))
         {
             if (event.type == Event::Closed)
-                window->close();
+                window.close();
         }
 
-        for (CircleShape shape : shapes_list)
-        {
-            window->clear();
-            srand(time(NULL));
+        // for (CircleShape shape : shapes_list)
+        // {
+        //     window.clear();
+        //     srand(time(NULL));
 
-            /* generate secret number between 1 and 10: */
-            icolor = rand() % 3;
-            iSecret = rand() % 3;
-            //x = rand() % WIDTH + 1;
-            // y = rand() % HEIGHT + 1;
-            //x, y = x + 10.f, y + 10.f;
-            y = 0.5f;
-            x += 5.f;
+        //     /* generate secret number between 1 and 10: */
+        //     icolor = rand() % 3;
+        //     iSecret = rand() % 3;
+        //     //x = rand() % WIDTH + 1;
+        //     // y = rand() % HEIGHT + 1;
+        //     //x, y = x + 10.f, y + 10.f;
+        //     y = 0.5f;
+        //     x += 5.f;
 
-            shape.setFillColor(colors_shapes[icolor]);
-            shape.setPosition(sf::Vector2(x, y));
-            shape.move(x, y);
-            window->draw(shape);
-            window->display();
-        }
+        //     shape.setFillColor(colors_shapes[icolor]);
+        //     shape.setPosition(sf::Vector2(x, y));
+        //     shape.move(x, y);
+        //     window.draw(shape);
+        // }
+            window.display();
     }
 
     return 0;
