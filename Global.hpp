f9e29c83d2b077f777db0f7f22bdf5f4baf572d3
Jonnas Costa <jonn567@gmail.com>
2021-11-21 18:57:21 +0000

    refactor repo and update structure

--- Global.hpp
+++ Global.hpp
@@ -2,7 +2,7 @@
 #include <SFML/Graphics.hpp>
 
 const int CELL_SIZE = 15;
-const unsigned int WIDTH {200}, HEIGHT{200};
+const unsigned int WIDTH {400}, HEIGHT{200};
 const int COLUMNS = WIDTH / CELL_SIZE;
 const int ROWS = HEIGHT / CELL_SIZE;
 
--- main.cc
+++ main.cc
@@ -1,11 +1,24 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window/WindowHandle.hpp>
 
 #include "Global.hpp"
 #include "classes/Field.hpp"
 
+sf::Font font;
+
+int init()
+{
+  if (!font.loadFromFile("./DroidSansMono.ttf"))
+  {
+    std::cerr << ".Error while loading font" << std::endl;
+    return -1;
+  }
+}
+
 int main()
 {
+  init();
   sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Example", sf::Style::Titlebar);
   jx::Field field;
   mouse_data md;
@@ -27,7 +40,7 @@ int main()
         md.pos = sf::Vector2i(m_x, m_y);
       }
 
-      if(event.type == sf::Event::MouseButtonReleased)
+      if (event.type == sf::Event::MouseButtonReleased)
         md.state = 0;
     }
 
@@ -35,6 +48,18 @@ int main()
     field.set_mouse_data(md);
     //pass by reference window in order to draw inside the field class
     field.draw_field(window);
+
+    // Declare and load a font
+    // Create a text
+    sf::Text text("hello", font);
+    text.setCharacterSize(30);
+    text.setStyle(sf::Text::Bold);
+    text.setCharacterSize(24); // in pixels, not points!
+    text.setPosition(WIDTH / 2, HEIGHT / 2);
+    text.setFillColor(sf::Color::Red);
+    // Draw it
+    window.draw(text);
+
     window.display();
   }
 
